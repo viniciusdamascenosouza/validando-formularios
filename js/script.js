9de917d526371f7b35c3b3895fc6b0e2e6a52c22
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-03-08 13:25:30 -0300

    AddEventListener

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+camposDoFormulario.forEach((campo) => {
+  campo.addEventListener("blur", () => verificaCampo(campo));
+});
+
+function verificaCampo(campo) {
+    
+}
