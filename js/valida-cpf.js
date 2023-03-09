24d2ff640d7c1a4199968df685303e147bfb7ec7
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-03-09 07:44:01 -0300

    Validando segundo digito do cpf

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,8 +1,14 @@
 export default function eUmCpf(campo) {
   const cpf = campo.value.replace(/\.|-/g, "");
-  validaNumerosRepetidos(cpf);
-  validaPrimeiroDigito(cpf);
-  console.log(validaNumerosRepetidos(cpf));
+  if (
+    validaNumerosRepetidos(cpf) ||
+    validaPrimeiroDigito(cpf) ||
+    validaSegundoDigito(cpf)
+  ) {
+    console.log("Esse CPF não existe!");
+  } else {
+    console.log("CPF existente!");
+  }
 }
 
 function validaNumerosRepetidos(cpf) {
@@ -35,4 +41,20 @@ function validaPrimeiroDigito(cpf) {
     soma = 0;
   }
   return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+  let soma = 0;
+  let multiplicador = 11;
+
+  for (let tamanho = 0; tamanho < 10; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+  soma = (soma * 10) % 11;
+
+  if (soma == 10 || soma == 11) {
+    soma = 0;
+  }
+  return soma != cpf[10];
 }
