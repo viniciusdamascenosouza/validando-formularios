d600c9ff5e83eddf5e35215b4fdba66a2215b788
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-03-09 07:19:09 -0300

    Validando o pirmeiro digito do cpf

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,7 +1,7 @@
 export default function eUmCpf(campo) {
   const cpf = campo.value.replace(/\.|-/g, "");
   validaNumerosRepetidos(cpf);
-
+  validaPrimeiroDigito(cpf);
   console.log(validaNumerosRepetidos(cpf));
 }
 
@@ -22,8 +22,17 @@ function validaNumerosRepetidos(cpf) {
 }
 
 function validaPrimeiroDigito(cpf) {
-    let soma = 0;
-    let multiplicador = 10;
+  let soma = 0;
+  let multiplicador = 10;
 
-    
+  for (let tamanho = 0; tamanho < 9; tamanho++) {
+    soma += cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+  soma = (soma * 10) % 11;
+
+  if (soma == 10 || soma == 11) {
+    soma = 0;
+  }
+  return soma != cpf[9];
 }
